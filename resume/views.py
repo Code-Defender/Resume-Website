f9e5b26569cd6bc5143027f40b793b9c104d01f2
Code-Defender <121310353+Code-Defender@users.noreply.github.com>
2026-05-23 18:05:38 +0500

    About and Contact page is added

--- resume/views.py
+++ resume/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 def home(request):
     return render(request, 'resume/home.html')
+
+def about(request):
+    return render(request, 'resume/about.html')
+
+def contact(request):
+    return render(request, 'resume/contact.html')
 # Create your views here.
